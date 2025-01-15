43770c1a75741570410eeff7768e13467fde02cb
yarrachandu7@gmail.com <yarrachandu7@gmail.com>
2025-01-15 22:30:09 +0530

    deploy

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM maven:3.8.1-openjdk-17 As build
+COPY . .
+RUN mvn clean package -DskipTests
+
+FROM openjdk:17.0.1-jdk-slim
+COPY --from=build /target/login-page-0.0.1-SNAPSHOT.jar chandubikshu.jar  
+EXPOSE 8080
+ENTRYPOINT ["java","-jar","chandubikshu.jar"]
---
+++ src/main/java/com/chandu/ChandubikshuApplication.java
@@ -0,0 +1,13 @@
+package com.chandu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChandubikshuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChandubikshuApplication.class, args);
+	}
+
+}
